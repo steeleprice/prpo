79d231196b95db04d2cce1c9bd0ebda958024664
Steele Price <stprice@courts.az.gov>
2021-01-02 19:57:14 -0700

    more structure changes

--- .devcontainer/scripts/setup-dotnet.sh
+++
@@ -1,8 +0,0 @@
-# Install .NET SDK
-# we use APT over curl, if you want nightlies, make a branch.
-# get Microsoft Repos
-wget https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb 
-dpkg -i packages-microsoft-prod.deb 
-apt-get update 
-apt-get -y install --no-install-recommends dotnet-sdk-3.1 dotnet-sdk-5.0 powershell
-rm packages-microsoft-prod.deb
